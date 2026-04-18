de30a97b5a4ff16bd6176df8c03fd9675f9e8c30
Sean Kirkpatrick <seanvokirkpatrick@gmail.com>
2026-04-18 18:05:45 -0400

    multiMiR analysis with target genes

---
+++ R/multi_mir_analysis.r
@@ -0,0 +1,247 @@
+run_multimir_query <- function(mirna_id, table_name, summary = FALSE) {
+  output_connection <- file(nullfile(), open = "wt")
+  message_connection <- file(nullfile(), open = "wt")
+
+  on.exit(
+    {
+      sink(type = "message")
+      sink()
+      close(message_connection)
+      close(output_connection)
+    },
+    add = TRUE
+  )
+
+  sink(output_connection)
+  sink(message_connection, type = "message")
+
+  multiMiR::get_multimir(
+    org = "hsa",
+    mirna = mirna_id,
+    table = table_name,
+    summary = summary
+  )
+}
+
+add_query_metadata <- function(query_row, data_frame) {
+  if (!nrow(data_frame)) {
+    return(data_frame)
+  }
+
+  data.frame(
+    miRNA = query_row$miRNA[[1]],
+    Annotation = query_row$Annotation[[1]],
+    multiMiR_query_id = query_row$multiMiR_query_id[[1]],
+    n_models_selected = query_row$n_models_selected[[1]],
+    data_frame,
+    row.names = NULL,
+    check.names = FALSE,
+    stringsAsFactors = FALSE
+  )
+}
+
+summary_column <- function(data_frame, column_name) {
+  if (column_name %in% names(data_frame)) {
+    data_frame[[column_name]]
+  } else {
+    rep(NA, nrow(data_frame))
+  }
+}
+
+summarize_validated_targets <- function(query_row, validated_multimir_summary) {
+  validated_target_summary <- data.frame(
+    miRNA = query_row$miRNA[[1]],
+    Annotation = query_row$Annotation[[1]],
+    multiMiR_query_id = query_row$multiMiR_query_id[[1]],
+    n_models_selected = query_row$n_models_selected[[1]],
+    target_symbol = summary_column(validated_multimir_summary, "target_symbol"),
+    target_entrez = summary_column(validated_multimir_summary, "target_entrez"),
+    target_ensembl = summary_column(validated_multimir_summary, "target_ensembl"),
+    validated_support = summary_column(validated_multimir_summary, "validated.sum"),
+    total_support = summary_column(validated_multimir_summary, "all.sum"),
+    row.names = NULL,
+    check.names = FALSE,
+    stringsAsFactors = FALSE
+  )
+
+  validated_target_summary <- validated_target_summary[
+    !is.na(validated_target_summary$target_symbol) &
+      validated_target_summary$target_symbol != "",
+    ,
+    drop = FALSE
+  ]
+
+  unique(validated_target_summary)
+}
+
+summarize_disease_drug <- function(query_row, disease_multimir_data) {
+  if (!nrow(disease_multimir_data)) {
+    return(data.frame())
+  }
+
+  disease_groups <- split(disease_multimir_data, disease_multimir_data$disease_drug)
+
+  data.frame(
+    miRNA = query_row$miRNA[[1]],
+    Annotation = query_row$Annotation[[1]],
+    multiMiR_query_id = query_row$multiMiR_query_id[[1]],
+    n_models_selected = query_row$n_models_selected[[1]],
+    disease_drug = names(disease_groups),
+    n_records = vapply(disease_groups, nrow, integer(1)),
+    n_databases = vapply(
+      disease_groups,
+      function(group_df) dplyr::n_distinct(group_df$database),
+      integer(1)
+    ),
+    n_pubmed = vapply(
+      disease_groups,
+      function(group_df) dplyr::n_distinct(group_df$paper_pubmedID[group_df$paper_pubmedID != ""], na.rm = TRUE),
+      integer(1)
+    ),
+    row.names = NULL,
+    check.names = FALSE,
+    stringsAsFactors = FALSE
+  )
+}
+
+has_rows <- function(data_frame) {
+  nrow(data_frame) > 0
+}
+
+run_multi_mir_analysis <- function(miRNA_details) {
+  consensus_miRNA_details <- miRNA_details[
+    !duplicated(miRNA_details$miRNA),
+    ,
+    drop = FALSE
+  ]
+
+  queried_miRNA <- consensus_miRNA_details[
+    !is.na(consensus_miRNA_details$multiMiR_query_id) &
+      nzchar(consensus_miRNA_details$multiMiR_query_id),
+    ,
+    drop = FALSE
+  ]
+
+  multimir_results <- list(
+    consensus_miRNA = consensus_miRNA_details,
+    queried_miRNA = queried_miRNA,
+    skipped_query_count = nrow(consensus_miRNA_details) - nrow(queried_miRNA),
+    validated = list(
+      per_query = list(),
+      raw_data = data.frame(),
+      summary = data.frame(),
+      total_record_count = 0L
+    ),
+    disease_drug = list(
+      per_query = list(),
+      raw_data = data.frame(),
+      summary = data.frame(),
+      total_record_count = 0L
+    )
+  )
+
+  if (nrow(queried_miRNA) == 0) {
+    return(multimir_results)
+  }
+
+  validated_per_query <- vector("list", nrow(queried_miRNA))
+  disease_per_query <- vector("list", nrow(queried_miRNA))
+  validated_raw_data_list <- vector("list", nrow(queried_miRNA))
+  validated_summary_list <- vector("list", nrow(queried_miRNA))
+  disease_raw_data_list <- vector("list", nrow(queried_miRNA))
+  disease_summary_list <- vector("list", nrow(queried_miRNA))
+
+  names(validated_per_query) <- queried_miRNA$miRNA
+  names(disease_per_query) <- queried_miRNA$miRNA
+
+  for (query_index in seq_len(nrow(queried_miRNA))) {
+    query_row <- queried_miRNA[query_index, , drop = FALSE]
+    query_id <- query_row$multiMiR_query_id[[1]]
+
+    validated_multimir <- run_multimir_query(
+      mirna_id = query_id,
+      table_name = "validated",
+      summary = TRUE
+    )
+    validated_multimir_data <- as.data.frame(validated_multimir@data)
+    validated_multimir_summary <- as.data.frame(validated_multimir@summary)
+
+    disease_multimir <- run_multimir_query(
+      mirna_id = query_id,
+      table_name = "disease.drug",
+      summary = FALSE
+    )
+    disease_multimir_data <- as.data.frame(disease_multimir@data)
+    disease_multimir_data <- disease_multimir_data[
+      !is.na(disease_multimir_data$disease_drug) &
+        disease_multimir_data$disease_drug != "",
+      ,
+      drop = FALSE
+    ]
+
+    validated_per_query[[query_index]] <- list(
+      query = query_row,
+      data = validated_multimir_data,
+      summary = validated_multimir_summary
+    )
+    disease_per_query[[query_index]] <- list(
+      query = query_row,
+      data = disease_multimir_data
+    )
+
+    validated_raw_data_list[[query_index]] <- add_query_metadata(query_row, validated_multimir_data)
+    validated_summary_list[[query_index]] <- summarize_validated_targets(query_row, validated_multimir_summary)
+    disease_raw_data_list[[query_index]] <- add_query_metadata(query_row, disease_multimir_data)
+    disease_summary_list[[query_index]] <- summarize_disease_drug(query_row, disease_multimir_data)
+  }
+
+  validated_raw_data_list <- Filter(has_rows, validated_raw_data_list)
+  validated_summary_list <- Filter(has_rows, validated_summary_list)
+  disease_raw_data_list <- Filter(has_rows, disease_raw_data_list)
+  disease_summary_list <- Filter(has_rows, disease_summary_list)
+
+  multimir_results$validated$per_query <- validated_per_query
+  multimir_results$disease_drug$per_query <- disease_per_query
+
+  if (length(validated_raw_data_list) > 0) {
+    multimir_results$validated$raw_data <- dplyr::bind_rows(validated_raw_data_list)
+    multimir_results$validated$total_record_count <- nrow(multimir_results$validated$raw_data)
+  }
+
+  if (length(validated_summary_list) > 0) {
+    multimir_results$validated$summary <- dplyr::bind_rows(validated_summary_list)
+    multimir_results$validated$summary <- multimir_results$validated$summary[
+      order(
+        -multimir_results$validated$summary$n_models_selected,
+        -multimir_results$validated$summary$validated_support,
+        -multimir_results$validated$summary$total_support,
+        multimir_results$validated$summary$miRNA,
+        multimir_results$validated$summary$target_symbol
+      ),
+      ,
+      drop = FALSE
+    ]
+  }
+
+  if (length(disease_raw_data_list) > 0) {
+    multimir_results$disease_drug$raw_data <- dplyr::bind_rows(disease_raw_data_list)
+    multimir_results$disease_drug$total_record_count <- nrow(multimir_results$disease_drug$raw_data)
+  }
+
+  if (length(disease_summary_list) > 0) {
+    multimir_results$disease_drug$summary <- dplyr::bind_rows(disease_summary_list)
+    multimir_results$disease_drug$summary <- multimir_results$disease_drug$summary[
+      order(
+        -multimir_results$disease_drug$summary$n_models_selected,
+        -multimir_results$disease_drug$summary$n_records,
+        -multimir_results$disease_drug$summary$n_databases,
+        multimir_results$disease_drug$summary$miRNA,
+        multimir_results$disease_drug$summary$disease_drug
+      ),
+      ,
+      drop = FALSE
+    ]
+  }
+
+  multimir_results
+}
